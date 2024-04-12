9faacd9871633b8429f7d3c16bc4077de864cba8
dnhuan <huandngoc@gmail.com>
2024-04-12 00:41:29 -0700

    Compact binary

--- api/screen.js
+++ api/screen.js
@@ -1,8 +1,40 @@
 import puppeteer from "puppeteer";
 import sharp from "sharp";
-import Jimp from "jimp";
+
 import chromium from "@sparticuz/chromium";
 chromium.setHeadlessMode = true;
+
+const SCREEN_WIDTH = 960;
+const SCREEN_HEIGHT = 540;
+
+function compactBuffer(binaryData) {
+	const pixels = new Uint8Array(binaryData);
+
+	// Calculate byte size for output array
+	const outputSize = Math.ceil(SCREEN_WIDTH / 2) * SCREEN_HEIGHT;
+
+	// Prepare output buffer
+	const outputBuffer = Buffer.alloc(outputSize);
+
+	for (let y = 0; y < SCREEN_HEIGHT; y++) {
+		let byte = 0;
+		for (let x = 0; x < SCREEN_WIDTH; x++) {
+			const pixelIndex = y * SCREEN_WIDTH + x;
+			const pixelValue = pixels[pixelIndex];
+			if (x % 2 === 0) {
+				byte = pixelValue >> 4;
+			} else {
+				byte |= pixelValue & 0xf0;
+				outputBuffer.writeUInt8(
+					byte,
+					y * Math.ceil(SCREEN_WIDTH / 2) + Math.floor(x / 2)
+				);
+			}
+		}
+	}
+
+	return outputBuffer;
+}
 
 export async function GET(request) {
 	try {
@@ -28,24 +60,24 @@ export async function GET(request) {
 			clip: {
 				x: 0,
 				y: 0,
-				width: 540,
-				height: 960,
+				width: SCREEN_HEIGHT,
+				height: SCREEN_WIDTH,
 			},
 		});
 		await browser.close();
 
 		const { searchParams } = new URL(request.url);
 		const hasDebug = searchParams.has("debug");
-		const debug = hasDebug ? searchParams.get("debug") === "true" : false;
+		const debug = hasDebug ? parseInt(searchParams.get("debug")) : 0;
 
 		const bitmap = sharp(screenshot)
 			.rotate(270)
-			.resize(960, 540)
+			.resize(SCREEN_WIDTH, SCREEN_HEIGHT)
 			.greyscale()
 			.extractChannel(1)
 			.raw({ depth: "uchar" });
 
-		if (debug) {
+		if (debug === 1) {
 			const exportImage = await bitmap.png().toBuffer();
 			return new Response(exportImage, {
 				headers: {
@@ -53,8 +85,18 @@ export async function GET(request) {
 				},
 			});
 		}
+
 		const exportBitmap = await bitmap.toBuffer();
-		return new Response(exportBitmap, {
+
+		if (debug === 2) {
+			return new Response(exportBitmap, {
+				headers: {
+					"Content-Type": "application/octet-stream",
+				},
+			});
+		}
+		const exportBufferArray = compactBuffer(exportBitmap);
+		return new Response(exportBufferArray, {
 			headers: {
 				"Content-Type": "application/octet-stream",
 			},
